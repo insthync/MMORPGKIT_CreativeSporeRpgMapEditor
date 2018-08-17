5033d5df8bc8ea40043541301f6fe51bcc7f804d
Ittipon Teerapruettikulchai <duckjump.comp@gmail.com>
2018-08-17 11:24:19 +0700

    Prepare classes to integration

---
+++ Scripts/CSCharacterModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreativeSpore.RpgMapEditor;
+
+namespace MultiplayerARPG
+{
+    [RequireComponent(typeof(PhysicCharBehaviour))]
+    public class CSCharacterModel : CharacterModel
+    {
+        private PhysicCharBehaviour cachePhysicCharBehaviour;
+        public PhysicCharBehaviour CachePhysicCharBehaviour
+        {
+            get
+            {
+                if (cachePhysicCharBehaviour == null)
+                    cachePhysicCharBehaviour = GetComponent<PhysicCharBehaviour>();
+                return cachePhysicCharBehaviour;
+            }
+        }
+
+        public override void UpdateAnimation(bool isDead, Vector3 moveVelocity, float playMoveSpeedMultiplier = 1)
+        {
+        }
+
+        public override Coroutine PlayActionAnimation(int actionId, AnimActionType animActionType, float playSpeedMultiplier = 1)
+        {
+            return null;
+        }
+
+        public override void PlayHurtAnimation()
+        {
+        }
+
+        public override void PlayJumpAnimation()
+        {
+        }
+    }
+}
---
+++ Scripts/CSPlayerCharacterEntity.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreativeSpore.RpgMapEditor;
+
+namespace MultiplayerARPG
+{
+    [RequireComponent(typeof(PhysicCharBehaviour))]
+    [RequireComponent(typeof(CSCharacterModel))]
+    public partial class CSPlayerCharacterEntity : BasePlayerCharacterEntity
+    {
+        private PhysicCharBehaviour cachePhysicCharBehaviour;
+        public PhysicCharBehaviour CachePhysicCharBehaviour
+        {
+            get
+            {
+                if (cachePhysicCharBehaviour == null)
+                    cachePhysicCharBehaviour = GetComponent<PhysicCharBehaviour>();
+                return cachePhysicCharBehaviour;
+            }
+        }
+
+        public override float StoppingDistance
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public override bool IsMoving()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void KeyMovement(Vector3 direction, bool isJump)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void PointClickMovement(Vector3 position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void StopMove()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
