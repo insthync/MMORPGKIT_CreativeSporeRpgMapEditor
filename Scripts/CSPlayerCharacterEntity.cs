08ba2eca10e50623973c0fab6878e7a71ab5d537
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-26 05:48:38 +0700

    Untested Player Character Entity

--- Scripts/CSPlayerCharacterEntity.cs
+++ Scripts/CSPlayerCharacterEntity.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CreativeSpore.RpgMapEditor;
+using UnityEngine.Profiling;
 
 namespace MultiplayerARPG
 {
@@ -9,6 +10,25 @@ namespace MultiplayerARPG
     [RequireComponent(typeof(CharacterModel2D))]
     public partial class CSPlayerCharacterEntity : BasePlayerCharacterEntity
     {
+        #region Settings
+        [Header("Movement AI")]
+        [Range(0.01f, 1f)]
+        public float stoppingDistance = 0.1f;
+        #endregion
+
+        #region Temp data
+        protected Collider2D[] overlapColliders2D = new Collider2D[OVERLAP_COLLIDER_SIZE];
+        protected Vector2 currentDirection;
+        protected Vector2? currentDestination;
+        #endregion
+
+        public Vector2 moveDirection { get; protected set; }
+
+        public override float StoppingDistance
+        {
+            get { return stoppingDistance; }
+        }
+
         private PhysicCharBehaviour cachePhysicCharBehaviour;
         public PhysicCharBehaviour CachePhysicCharBehaviour
         {
@@ -20,32 +40,91 @@ namespace MultiplayerARPG
             }
         }
 
-        public override float StoppingDistance
+        protected override void EntityAwake()
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+            base.EntityAwake();
+            StopMove();
         }
 
-        public override bool IsMoving()
+        protected override void EntityUpdate()
         {
-            throw new System.NotImplementedException();
+            base.EntityUpdate();
+            Profiler.BeginSample("PlayerCharacterEntity2D - Update");
+            if (IsDead())
+            {
+                StopMove();
+                SetTargetEntity(null);
+                return;
+            }
+            Profiler.EndSample();
+        }
+
+        protected override void EntityFixedUpdate()
+        {
+            base.EntityFixedUpdate();
+            Profiler.BeginSample("PlayerCharacterEntity2D - FixedUpdate");
+            if (currentDestination.HasValue)
+            {
+                var currentPosition = new Vector2(CacheTransform.position.x, CacheTransform.position.y);
+                moveDirection = (currentDestination.Value - currentPosition).normalized;
+                if (Vector3.Distance(currentDestination.Value, currentPosition) < StoppingDistance)
+                    StopMove();
+            }
+
+            if (!IsDead())
+            {
+                var moveDirectionMagnitude = moveDirection.magnitude;
+                if (!IsPlayingActionAnimation() && moveDirectionMagnitude != 0)
+                {
+                    if (moveDirectionMagnitude > 1)
+                        moveDirection = moveDirection.normalized;
+
+                    CachePhysicCharBehaviour.Dir = moveDirection;
+                    CachePhysicCharBehaviour.MaxSpeed = CacheMoveSpeed;
+                }
+            }
+            Profiler.EndSample();
         }
 
         public override void KeyMovement(Vector3 direction, bool isJump)
         {
-            throw new System.NotImplementedException();
+            if (IsDead())
+                return;
+            moveDirection = direction;
+            if (moveDirection.magnitude == 0)
+                CachePhysicCharBehaviour.Dir = Vector2.zero;
         }
 
         public override void PointClickMovement(Vector3 position)
         {
-            throw new System.NotImplementedException();
+            if (IsDead())
+                return;
+            currentDestination = position;
         }
 
         public override void StopMove()
         {
-            throw new System.NotImplementedException();
+            currentDestination = null;
+            moveDirection = Vector3.zero;
+            CachePhysicCharBehaviour.Dir = Vector2.zero;
+        }
+
+        protected override int OverlapObjects(Vector3 position, float distance, int layerMask)
+        {
+            return Physics2D.OverlapCircleNonAlloc(position, distance, overlapColliders2D, layerMask);
+        }
+
+        protected override GameObject GetOverlapObject(int index)
+        {
+            return tempGameObject = overlapColliders2D[index].gameObject;
+        }
+
+        protected override bool IsPositionInAttackFov(float fov, Vector3 position)
+        {
+            var halfFov = fov * 0.5f;
+            var angle = Vector2.Angle((CacheTransform.position - position).normalized, currentDirection);
+            // Angle in forward position is 180 so we use this value to determine that target is in hit fov or not
+            return (angle < 180 + halfFov && angle > 180 - halfFov);
         }
     }
 }
