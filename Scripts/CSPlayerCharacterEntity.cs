16766cdc302d72b66795bf21f5ae502c2915ae53
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-30 03:07:40 +0700

    Extends from PlayerCharacterEntity2D to make changes only if it needed

--- Scripts/CSPlayerCharacterEntity.cs
+++ Scripts/CSPlayerCharacterEntity.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using CreativeSpore.RpgMapEditor;
 using UnityEngine.Profiling;
+using LiteNetLibManager;
 
 namespace MultiplayerARPG
 {
     [RequireComponent(typeof(PhysicCharBehaviour))]
     [RequireComponent(typeof(CharacterModel2D))]
-    public partial class CSPlayerCharacterEntity : BasePlayerCharacterEntity
+    public partial class CSPlayerCharacterEntity : PlayerCharacterEntity2D
     {
-        #region Settings
-        [Header("Movement AI")]
-        [Range(0.01f, 1f)]
-        public float stoppingDistance = 0.1f;
-        #endregion
-
-        #region Temp data
-        protected Collider2D[] overlapColliders2D = new Collider2D[OVERLAP_COLLIDER_SIZE];
-        protected Vector2 currentDirection;
-        protected Vector2? currentDestination;
-        #endregion
-
-        public Vector2 moveDirection { get; protected set; }
 
         public override float StoppingDistance
         {
@@ -40,29 +28,13 @@ namespace MultiplayerARPG
             }
         }
 
-        protected override void EntityAwake()
-        {
-            base.EntityAwake();
-            StopMove();
-        }
-
-        protected override void EntityUpdate()
-        {
-            base.EntityUpdate();
-            Profiler.BeginSample("PlayerCharacterEntity2D - Update");
-            if (IsDead())
-            {
-                StopMove();
-                SetTargetEntity(null);
-                return;
-            }
-            Profiler.EndSample();
-        }
-
         protected override void EntityFixedUpdate()
         {
             base.EntityFixedUpdate();
             Profiler.BeginSample("PlayerCharacterEntity2D - FixedUpdate");
+            if (!IsServer && !IsOwnerClient)
+                return;
+
             if (currentDestination.HasValue)
             {
                 var currentPosition = new Vector2(CacheTransform.position.x, CacheTransform.position.y);
@@ -78,53 +50,29 @@ namespace MultiplayerARPG
                 {
                     if (moveDirectionMagnitude > 1)
                         moveDirection = moveDirection.normalized;
-
+                    UpdateCurrentDirection(moveDirection);
                     CachePhysicCharBehaviour.Dir = moveDirection;
                     CachePhysicCharBehaviour.MaxSpeed = CacheMoveSpeed;
+                }
+
+                RpgNetworkEntity tempEntity;
+                if (moveDirectionMagnitude == 0 && TryGetTargetEntity(out tempEntity))
+                {
+                    var targetDirection = (tempEntity.CacheTransform.position - CacheTransform.position).normalized;
+                    if (targetDirection.magnitude != 0f)
+                        UpdateCurrentDirection(targetDirection);
                 }
             }
             Profiler.EndSample();
         }
-
-        public override void KeyMovement(Vector3 direction, bool isJump)
-        {
-            if (IsDead())
-                return;
-            moveDirection = direction;
-            if (moveDirection.magnitude == 0)
-                CachePhysicCharBehaviour.Dir = Vector2.zero;
-        }
-
-        public override void PointClickMovement(Vector3 position)
-        {
-            if (IsDead())
-                return;
-            currentDestination = position;
-        }
-
+        
         public override void StopMove()
         {
             currentDestination = null;
             moveDirection = Vector3.zero;
             CachePhysicCharBehaviour.Dir = Vector2.zero;
-        }
-
-        public override int OverlapObjects(Vector3 position, float distance, int layerMask)
-        {
-            return Physics2D.OverlapCircleNonAlloc(position, distance, overlapColliders2D, layerMask);
-        }
-
-        public override GameObject GetOverlapObject(int index)
-        {
-            return tempGameObject = overlapColliders2D[index].gameObject;
-        }
-
-        public override bool IsPositionInFov(float fov, Vector3 position)
-        {
-            var halfFov = fov * 0.5f;
-            var angle = Vector2.Angle((CacheTransform.position - position).normalized, currentDirection);
-            // Angle in forward position is 180 so we use this value to determine that target is in hit fov or not
-            return (angle < 180 + halfFov && angle > 180 - halfFov);
+            if (IsOwnerClient && !IsServer)
+                CallNetFunction("StopMove", FunctionReceivers.Server);
         }
     }
 }
